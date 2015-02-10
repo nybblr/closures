3c9de66285e1b1892e5e3d4398a20eadca9fd12f
Jonathan Martin <me@nybblr.com>
2015-02-09 19:05:06 -0500

    Add IIFE specs.

--- specs.js
+++ specs.js
@@ -1,5 +1,39 @@
 describe('Scope', function () {
-  it('should work', function () {
+  it('should work with IIFEs', function () {
+    expect(g('a')).to.eql(undefined);
+    expect(g('b')).to.eql(undefined);
+    expect(g('c')).to.eql(undefined);
+
+    f('outer', ['a', 'b'], function () {
+
+      expect(g('a')).to.eql(1);
+      expect(g('b')).to.eql(2);
+      expect(g('c')).to.eql(undefined);
+
+      f('inner', ['a', 'c'], function () {
+
+        expect(g('a')).to.eql(3);
+        expect(g('b')).to.eql(2);
+        expect(g('c')).to.eql(4);
+
+      });
+
+      g('inner')(3,4);
+
+      expect(g('a')).to.eql(1);
+      expect(g('b')).to.eql(2);
+      expect(g('c')).to.eql(undefined);
+
+    });
+
+    g('outer')(1,2);
+
+    expect(g('a')).to.eql(undefined);
+    expect(g('b')).to.eql(undefined);
+    expect(g('c')).to.eql(undefined);
+  });
+
+  it('should remember scope when closure is invoked outside', function () {
     f('outer', ['x', 'y'], function () {
       f('inner', ['x', 'z'], function () {
         return [g('x'), g('y'), g('z')];
