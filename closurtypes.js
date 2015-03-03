7323dc403e55be9f64675c234596ad395b225e74
Jonathan Martin <me@nybblr.com>
2015-03-02 22:00:57 -0500

    Nuke final closurtypes, just use the main file.

--- closurtypes.js
+++ closurtypes.js
@@ -1,23 +1,52 @@
-var Scope = (function() {
-  function Scope() {
-    this._dict = {};
+var ClosureRegistry = (function() {
+  function ClosureRegistry() {
+    this._registry = [new Scope()];
   }
 
-  Scope.prototype.push = function() {
-    this._dict = this._forkDict();
+  delegate(['get', 'set', 'args'], {
+    from: ClosureRegistry.prototype,
+    to: function() { return this.scopeForCurrentClosure(); }
+  });
+
+  delegate(['push', 'pop'], {
+    from: ClosureRegistry.prototype,
+    to: function() { return this._registry; }
+  });
+
+  ClosureRegistry.prototype.scopeForCurrentClosure = function() {
+    return this._registry[this._registry.length - 1];
   };
 
-  Scope.prototype.pop = function() {
-    this._dict = this._dict.__parent;
+  ClosureRegistry.prototype.func = function(name, params, body) {
+    var _this = this;
+    var scope = this.scopeForCurrentClosure().fork();
+    this.set(name, function() {
+      _this.push(scope);
+      _this.args(params, arguments);
+      var result = body();
+      _this.pop();
+      return result;
+    });
   };
+
+  return ClosureRegistry;
+}());
+
+var Scope = (function() {
+  function Scope(dict) {
+    this._dict = dict || {};
+  }
 
   Scope.prototype._forkDict = function() {
     var dict = this._dict;
     var F = function() {};
     F.prototype = Object.create(dict);
     F.prototype.constructor = F;
-    F.prototype.__parent = dict;
     return new F();
+  };
+
+  Scope.prototype.fork = function() {
+    return new Scope(this._forkDict());
   };
 
   Scope.prototype.get = function(key) {
@@ -26,6 +55,12 @@ var Scope = (function() {
 
   Scope.prototype.set = function(key, value) {
     this._dict[key] = value;
+  };
+
+  Scope.prototype.args = function(names, values) {
+    for (var i = 0, l = names.length; i < l; i ++) {
+      this.set(names[i], values[i]);
+    }
   };
 
   return Scope;
